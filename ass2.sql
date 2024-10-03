c3c428e87ecda4d1e482396e148d632aa2ade0d3
Maryna Kosse <150956151+Maryn7679@users.noreply.github.com>
2024-10-03 18:24:49 +0300

    Create ass2.sql

---
+++ ass2.sql
@@ -0,0 +1,43 @@
+create index idx on table2(price)
+
+with my_data1 as (select user_id, name, surname, price, registration_date, min(transaction_time) from 
+(select * from table1 where name = 'Anna') as t1
+inner join
+(select * from table2 where price > 5000 and is_available = 1) as t2
+on t1.item_bought = t2.item_id
+cross join (select * from table3 where transaction_type = 'not' and device = 'Device5') as t3
+where registration_date < transaction_time
+group by user_id, name, surname, price, registration_date
+)
+
+select * from my_data1
+where price < (select avg(price)+800 from my_data1)
+order by price
+
+~~~~
+drop index idx on table2
+
+select user_id, name, surname, price, registration_date, min(transaction_time) from table1 as t1
+inner join table2 as t2
+on t1.item_bought = t2.item_id
+cross join table3 as t3
+where name = 'Anna'
+and price > 5000 
+and is_available = 1
+and transaction_type = 'not' 
+and device = 'Device5'
+and registration_date < transaction_time
+group by user_id, name, surname, price, registration_date
+having price < (select avg(price)+800 from (
+	select user_id, name, surname, price, registration_date, min(transaction_time) from table1 as t1
+	inner join table2 as t2
+	on t1.item_bought = t2.item_id
+	cross join table3 as t3
+	where name = 'Anna'
+	and price > 5000 
+	and is_available = 1
+	and transaction_type = 'not' 
+	and device = 'Device5'
+	and registration_date < transaction_time
+	group by user_id, name, surname, price, registration_date) as t5)
+order by price
